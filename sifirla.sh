3f573f1ee5d8b943b8b9cf41c1f6f6695f6e9c48
Lizzard <101275325+lizzard-development@users.noreply.github.com>
2025-02-09 23:12:01 +0300

    Update sifirla.sh

--- sifirla.sh
+++ sifirla.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
-
-# Mevcut banner dosyalarını kaldır
-rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
-rm -rf /data/data/com.termux/files/usr/etc/zshrc
-
-# Termux'u varsayılana döndür
-apt remove toilet figlet lolcat nano -y
-apt autoremove -y
-apt update -y
-apt upgrade -y
-
-echo "Sıfırlama tamamlandı. Termux'u yeniden başlatın!"
+folder_remove() {
+cd $HOME
+if [[ -e Termux-Banner ]]
+then
+rm -rvf Termux-Banner
+fi
+}
+install() {
+echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then>         command_not_found_handle() {
+/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+fi
+PS1='\$ '" > /data/data/com.termux/files/usr/etc/zshrc
+echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+command_not_found_handle() {
+/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+fi
+PS1='\$ '" > /data/data/com.termux/files/usr/etc/bash.bashrc
+}
+folder_remove
+install
+folder_remove
